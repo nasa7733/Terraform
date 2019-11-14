0c0137acfa083bd875899b99f11d4419366f05fa
unknown <ishaqmd@live.com>
2019-11-14 09:50:36 +0530

    one

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "google" {
+  project = "silent-thunder-256105"
+  region  = "europe-west2"
+  zone    = "europe-west2-a"
+}
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_storage_bucket" "terraform_state" {
+  name          = "terraform_bkt"
+  force_destroy = true
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+ 
+ 
