f32b43ee6c842b7e9fdd26aca3b3b0628af6bfe2
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2020-03-24 19:31:43 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+# This is the provider used to spin up the gcloud instance
+provider "google" {
+  #credentials = file("terraform.json")
+  project = "lyrical-shore-262101"
+  region  = "us-east1"
+  
+}
+
+
+resource "google_compute_instance" "vm-instance" {
+  name         = "test"
+  machine_type = "f1-micro"
+  zone         = "us-east1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  metadata_startup_script = "sudo su && apt-get update && apt-get install git -y && apt-get install unzip -y && wget https://releases.hashicorp.com/terraform/0.12.0/terraform_0.12.0_linux_amd64.zip && unzip terraform_0.12.0_linux_amd64.zip && sudo mv terraform /usr/local/bin/ && terraform --version"
+
+   
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
