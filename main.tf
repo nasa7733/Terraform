72e76450625ec6dcd67481d0dff4225fe1ee6d00
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2020-03-27 09:35:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "google" {
 
 
 resource "google_compute_instance" "vm-instance" {
-  name         = "test"
+  name         = "test01"
   machine_type = "f1-micro"
   zone         = "us-east1-b"
 
@@ -18,7 +18,7 @@ resource "google_compute_instance" "vm-instance" {
     }
   }
 
-  metadata_startup_script = "sudo su && apt-get update && apt-get install git -y && apt-get install unzip -y && wget https://releases.hashicorp.com/terraform/0.12.0/terraform_0.12.0_linux_amd64.zip && unzip terraform_0.12.0_linux_amd64.zip && sudo mv terraform /usr/local/bin/ && terraform --version"
+  metadata_startup_script = "sudo su && apt-get update && apt-get install git -y && apt-get install unzip -y && wget https://releases.hashicorp.com/terraform/0.12.0/terraform_0.12.0_linux_amd64.zip && unzip terraform_0.12.0_linux_amd64.zip && sudo mv terraform /usr/local/bin/ && terraform --version https://github.com/nasa7733/GCE-STARTUP.git &&  cd /gce-startup &&  terraform init && terraform plan && terraform apply -input=false -auto-approve"
 
    
   network_interface {
