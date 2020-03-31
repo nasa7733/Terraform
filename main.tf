0808324655d55b40ff5d3ccedc36274bac5b2c02
nasa7733 <58895722+nasa7733@users.noreply.github.com>
2020-03-31 09:13:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "google_compute_instance" "vm-instance" {
     }
   }
 
-  metadata_startup_script = "sudo su && apt-get update && apt-get install git -y && apt-get install unzip -y && wget https://releases.hashicorp.com/terraform/0.12.0/terraform_0.12.0_linux_amd64.zip && unzip terraform_0.12.0_linux_amd64.zip && sudo mv terraform /usr/local/bin/ && terraform --version && git clone https://github.com/nasa7733/GCE-STARTUP.git  && wget https://storage.cloud.google.com/lyrical-shore-262101-pipeline/cred.json  && cp cred.json /GCE-STARTUP && cd /GCE-STARTUP &&   ls && terraform init && terraform plan && terraform apply -input=false -auto-approve" 
+  metadata_startup_script = "sudo su && apt-get update && apt-get install git -y && apt-get install unzip -y && wget https://releases.hashicorp.com/terraform/0.12.0/terraform_0.12.0_linux_amd64.zip && unzip terraform_0.12.0_linux_amd64.zip && sudo mv terraform /usr/local/bin/ && terraform --version && git clone https://github.com/nasa7733/GCE-STARTUP.git  && wget https://storage.googleapis.com/nasa-test-bucket/new_cred.json  && cp new_cred.json /GCE-STARTUP && cd /GCE-STARTUP &&   ls && terraform init && terraform plan && terraform apply -input=false -auto-approve" 
    
 
   network_interface {
